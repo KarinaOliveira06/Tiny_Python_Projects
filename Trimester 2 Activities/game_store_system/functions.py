f97f015d3d9775e5821a0c35b642332e45cf416e
Karina Gomes <karinaoliveira25050@gmail.com>
2026-05-15 15:42:15 -0300

    help function add

---
+++ Trimester 2 Activities/game_store_system/functions.py
@@ -0,0 +1,33 @@
+def help():
+    instructions = input("""
+    To login at the system, you need to create a new account.
+    To create a new account type 'register'.
+    To get some using instructions type 'instructions' or 'more help'
+    """)
+
+    try:
+
+        if instructions in ["register", "regis", "r"]:
+            employee = input("type your name: ")
+            password = input("type your password: ")
+            with open ("register.txt", "a", encoding = "UTF-8") as arquivo:
+                arquivo.write(f"Name: {employee}" "\n"
+                f"Password: {password}")
+                print(f"Congratulations {employee}! You registered your account, now try log in.")
+
+        elif instructions in ["instructions", "i", "ins", "more help", "help", "more", "morehelp"]:
+            input("""
+            After create an account, you need to login at first page;
+            When you are logged in, you can register a product selecting 'register product' (1) option;
+            You can see your products menu typing number 2;
+            To apply a discount, simply type the name of a product from the menu;
+            You can see the total value and total products typing 'total' at the menu.
+            """)
+        else:
+            print("Something went wrong, please try again.")
+
+    except ValueError:
+        print("Type a valid value.")
+
+    input("Press enter to continue.")
+        
--- Trimester 2 Activities/game_store_system/main.py
+++ Trimester 2 Activities/game_store_system/main.py
@@ -1,13 +1,21 @@
+from functions import help
+
 login = input("""
 Welcome to the World Games Store register system!
 
 If you are a new employee, type 'Help' to get instructions.
-If not, type 'Enter' to log in to the registration system.
+If not, type 'Login' to log in to the registration system.
+If you want to exit, type 'Exit'.
+
 """).lower()
 
 while True:
     try:
         if login in ["help", "hel", "he", "h"]:
+            help()
+        elif login in ["login", "log", "in", "log in", "l"]:
+            name_login = input("Type your name: ")
+            password_login = input("Type your password: ")
 
     except ValueError:
         print("Something went wrong, value error.")
