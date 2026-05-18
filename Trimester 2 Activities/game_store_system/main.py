c7a916336a5ab5e6f159852becbdec41bdad4d60
Karina Gomes <karinaoliveira25050@gmail.com>
2026-05-18 14:56:23 -0300

    register_function add

--- Trimester 2 Activities/game_store_system/main.py
+++ Trimester 2 Activities/game_store_system/main.py
@@ -1,6 +1,8 @@
-from functions import help
+from functions import help_function
+from functions import register_function
 
 while True:
+    login_successful = False
 
     login = input("""
     Welcome to the World Games Store register system!
@@ -11,16 +13,30 @@ while True:
 
     """).lower()
 
-        if login in ["help", "hel", "he", "h"]:
-            help()
-        elif login in ["login", "log", "in", "log in", "l"]:
-            name_login = input("Type your name: ")
-            password_login = input("Type your password: ")
+    if login in ["help", "hel", "he", "h"]:
+        help_function()
+    elif login in ["login", "log", "in", "log in", "l"]:
+        name_login = input("Type your name: ")
+        password_login = input("Type your password: ")
 
-            with open("register.txt", "r") as archive:
-                for line in archive:
-                        line = line.strip()
-                        data = line.split(",")
-                        if name_login == data[0] and password_login == data[1]:
-                            print("login Successful!")
-                            break
+        with open("register.txt", "r") as archive:
+            for line in archive:
+                    line = line.strip()
+                    data = line.split(",")
+                    if name_login == data[0] and password_login == data[1]:
+                        login_successful = True
+                        print("login Successful!")
+                        break
+
+    if login_successful == True:
+        break
+        system_select = input("""=== Game Store System ===
+        
+        (1) Register Product
+        (2) Products Menu
+        
+        """)
+        if  system_select in ["1", "register", "register product", "registerproduct", "regis", "re", "um", "one"]:
+            register_function()
+
+
