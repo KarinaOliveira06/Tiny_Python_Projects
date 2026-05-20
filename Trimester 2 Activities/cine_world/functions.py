ce7aa74e6245801789f9c71fd3ff83748a967205
Karina Gomes <karinaoliveira25050@gmail.com>
2026-05-20 16:25:59 -0300

    pos system add

--- Trimester 2 Activities/cine_world/functions.py
+++ Trimester 2 Activities/cine_world/functions.py
@@ -1,15 +1,45 @@
-def movie_register(select_movie):
+def clean_line():
+    with open("movies.txt", "r", encoding = "UTF-8") as archive:
+        movie_list = []
+        for line in archive:
+            data = line.split(",")
+            line = line.strip()
+
+            if len(data) == 2:
+                movie_list.append(data)
+            return movie_list
+
+def movie_register(select_movie, select_price):
     with open("movies.txt", "a", encoding = "UTF-8") as archive:
-            archive.write(f"{select_movie}\n")
+            archive.write(f"{select_movie},{select_price}\n")
 
 def digital_poster():
-    with open("movies.txt", "r", encoding = "UTF-8") as archive:
-        data = archive.read()
-        print(f"""
-(-movies-)
-{data}
-""")
+    print("---Movies---")
+    all_movies = clean_line()
+    for data in all_movies:
+            print(f"""
+    {data[0].title()} - ${data[1]}
+            """)
     return_menu = input("Press [ENTER] to return to the menu.")
 
-def pos_system():
-    insert_value = input("")
+def pos_system(buy_ticket, cart, total_value):
+    if buy_ticket in ["ticket", "tickets", "cricket", "tick", "t"]:
+        selected_movie = input("Type the movie name: ").lower()
+        ticket_amount = int(input("Tickets amount: "))
+        all_movies = clean_line()
+
+        for data in all_movies:
+            if selected_movie == data[0]:
+                ticket_price = int(data[1])
+                total_value = ticket_price * ticket_amount
+
+                print(f"""
+(--- cart ---)
+
+{data[0]} - ${total_value}
+
+                """)
+                break
+            else:
+                print("Invalid value, please try again.")
+
--- Trimester 2 Activities/cine_world/main.py
+++ Trimester 2 Activities/cine_world/main.py
@@ -1,26 +1,28 @@
-from functions import movie_register
-from functions import digital_poster
+from functions import digital_poster, pos_system, movie_register
 cart = []
 
 print("""
 Welcome to the Cine World System!
 """)
+while True:
+    system_select = input("""
+    --- Options ---
+    (1) Movie register
+    (2) Digital poster
+    (3) POS System
+    (4) Box office
+    (5) Exit
+    """).lower()
 
-system_select = input("""
---- Options ---
-(1) Movie register
-(2) Digital poster
-(3) POS System
-(4) Box office
-(5) Exit
-""").lower()
-
-if system_select in ["1", "movie", "movie register", "register", "reg", "regigigas"]:
-    select_movie = input("Type the movie name: ").lower()
-    movie_register(select_movie)
-    print(f"Congratulations, you've registered {select_movie}!")
-elif system_select in ["2", "digital" "poster", "digital", "poster", "post"]:
-    digital_poster()
-elif system_select in ["3", "pos", "pos system", "system", "sys", "ops", "pos sys", "possystem", "posys"]:
-    insert_value = input("Hello, type 'ticket' to buy tickets.")
-    pos_system(insert_value) 
+    if system_select in ["1", "movie", "movie register", "register", "reg", "regigigas"]:
+        select_movie = input("Type the movie name: ").lower()
+        select_price = input("Type the price: ").lower()
+        movie_register(select_movie, select_price)
+        print(f"Congratulations, you've registered {select_movie}!")
+    elif system_select in ["2", "digital" "poster", "digital", "poster", "post"]:
+        digital_poster()
+    elif system_select in ["3", "pos", "pos system", "system", "sys", "ops", "pos sys", "possystem", "posys"]:
+        buy_ticket = input("Hello, type 'ticket' to buy tickets: ").lower()
+        pos_system(buy_ticket, cart, total_value) 
+        "\n"
+        print(f"Congratulations, you got a ticket!")
