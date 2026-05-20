882ea669a4277debfeaf1a9122703eaa8607d689
Karina Gomes <karinaoliveira25050@gmail.com>
2026-05-20 16:46:00 -0300

    Final version

--- Trimester 2 Activities/cine_world/main.py
+++ Trimester 2 Activities/cine_world/main.py
@@ -1,4 +1,4 @@
-from functions import digital_poster, pos_system, movie_register
+from functions import digital_poster, pos_system, movie_register, box_office
 cart = []
 
 print("""
@@ -19,10 +19,15 @@ while True:
         select_price = input("Type the price: ").lower()
         movie_register(select_movie, select_price)
         print(f"Congratulations, you've registered {select_movie}!")
-    elif system_select in ["2", "digital" "poster", "digital", "poster", "post"]:
+    elif system_select in ["2", "digital", "poster", "post"]:
         digital_poster()
     elif system_select in ["3", "pos", "pos system", "system", "sys", "ops", "pos sys", "possystem", "posys"]:
         buy_ticket = input("Hello, type 'ticket' to buy tickets: ").lower()
-        pos_system(buy_ticket, cart, total_value) 
-        "\n"
-        print(f"Congratulations, you got a ticket!")
+        pos_system(buy_ticket, cart) 
+        print(f"\nCongratulations, you got a ticket!")
+    elif system_select in ["4", "four", "quatro", "box", "box office", "boxoffice", "box_office"]:
+        box_office(cart)
+    elif system_select in ["exit", "ex", "sair", "quit", "5", "cinco", "five"]:
+        break
+    else:
+        print("Value error, please try again.")
